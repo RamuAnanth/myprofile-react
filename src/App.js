6ca190aae7f38f784fb7eb0fe8735f88aa27c6dc
Ramu <ramuanantha6299@gmail.com>
2024-07-03 15:05:32 +0530

    myprofile reactjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Menu from './menu';
+import Footer from './Footer';
+import Particales from './components/Particales';
+import ContentPart from './ContentPart';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particales/>
+      <div className="header-menu-container">
+        <Menu/>
+      </div>
     </div>
   );
 }
